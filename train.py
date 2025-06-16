abf9d075cb77d601cca039a628f8f28859c920d6
amaljudelitto <amaljudelitto@gmail.com>
2025-06-16 14:57:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, TextDataset, DataCollatorForLanguageModeling
 import os
 
-model_name = "NousResearch/TinyLLaMA-1.1B-Chat-v1.0"
+model_name = "google/flan-t5-base"
+
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
