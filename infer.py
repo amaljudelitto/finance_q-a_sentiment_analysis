62323bd0222343854b79997e2f0dbb1c5b2a3134
amaljudelitto <amaljudelitto@gmail.com>
2026-04-25 00:17:14 +0530

    Change model loading to use Seq2SeqLM

--- infer.py
+++ infer.py
@@ -5,7 +5,7 @@ model_name = "google/flan-t5-base"
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 model.eval()
 
